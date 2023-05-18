274ba2be46e6f873e68d3ec6a842859ac51801d0
lyuting47 <90270519+lyuting47@users.noreply.github.com>
2023-05-18 15:56:19 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,15 @@ const root = createRoot(container!);
 
 root.render(
   <Auth0Provider
-    domain="INSERT YOUR AUTH0 DOMAIN"
-    clientId="INSERT YOUR APPLICATION CLIENT ID FROM AUTH0"
+    domain="INSERT YOUR AUTH0 DOMAIN"                             // To fill
+    clientId="INSERT YOUR APPLICATION CLIENT ID FROM AUTH0"       // To fill
     useRefreshTokens={true}
     useRefreshTokensFallback={true}
     cacheLocation='localstorage'
     authorizationParams={{
       redirect_uri: window.location.origin,
-      audience: "INSERT YOUR API IDENTIFIER FROM AUTH0",
-      scope: "openid profile email offline_access read:messages"
+      audience: "INSERT YOUR API IDENTIFIER FROM AUTH0",          // To fill
+      scope: "openid profile email offline_access read:messages"  // You may modify this to set the scope the logged in user will have
     }}
   >
     <App />
